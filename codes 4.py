01a85f29f219b39daf7e451e380504ca58009837
yousifmohameddev <yousifalaa20033@gmail.com>
2024-11-20 14:10:35 +0200

    Add files via upload

---
+++ codes 4.py
@@ -0,0 +1,23 @@
+#name = input('name: ').title()
+
+#print(f"welcome {name} at codezilla python course \n and your name is {len("name")} characters long")
+
+
+#num = 100*3
+#print(f"{num:.2f}")
+
+#order = 450
+#service = 450/10
+#total = (f"Enter order cost: 450\n the total is: {order+service:,} ")
+#print(total)
+
+#ask the user about weight in kg
+#convert from kg to gram
+#print weight in grams
+
+weight_kg= float(input("Enter the weight by kg:"))  #if forget 'float or int' will be  
+weight_gram = weight_kg * 1000                      #the result 'dublicate number' 
+print(f"{weight_kg} kg is {weight_gram:,} gram")      #Exampel x*5 = xxxxx  .  
+
+
+
